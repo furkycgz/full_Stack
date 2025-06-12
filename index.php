300f7f84788be893f67195d8019ac651e0fcacf1
furkycgz <126878085+furkycgz@users.noreply.github.com>
2025-06-12 17:22:10 +0300

    php_1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+function ucgenciz($sayi){
+     
+   for ($i=1; $i<= $sayi ; $i++) {
+         $sifir=$i ; 
+         for($k=1 ; $k<=$sifir; $k++ ){
+                 echo 0 ;
+                
+         }
+        echo "<br>";
+   }
+
+}
+ucgenciz(15);
+?>
