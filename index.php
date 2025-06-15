11a523c64677ced7c255d49976db8ff34a3e7b87
furkycgz <126878085+furkycgz@users.noreply.github.com>
2025-06-15 13:48:47 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
-<?php
-
-function ucgenciz($sayi){
-     
-   for ($i=1; $i<= $sayi ; $i++) {
-         $sifir=$i ; 
-         for($k=1 ; $k<=$sifir; $k++ ){
-                 echo 0 ;
-                
-         }
-        echo "<br>";
-   }
-
-}
-ucgenciz(15);
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <h2>Bir sayı Giriniz</h2>
+    <form method="post" action="p.php">
+        <label for="sayı">sayı:</label>
+        <input type="int" id="sayı" name="sayı"><br><br>
+        
+    
+        <input type="submit" value="Gönder">
+    </form>
+</body>
+</html>
